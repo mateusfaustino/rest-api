4c2ee17cf5fcf8da23f1e54c002e8a0ab74a16c3
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-07-20 12:36:55 -0300

    read/writeStream  file fs

--- files/filesUpload.js
+++ files/filesUpload.js
@@ -1,21 +1,11 @@
 const fs = require('fs')
-const path = './assets/pet01.jpg'
+const path = './assets/pet02.jpg'
 
 function handleError(error){
     console.log("Error------------------------------------");
     console.log(error);
 }
-fs.readFile(path,(error, buffer)=>{
-    if(error){
-        handleError(error)
-    }else{
-        console.log("Image was buffered");
-        fs.writeFile('./assets/pet01-copy.jpg',buffer,(error)=>{
-            if(error){
-                handleError(error)
-            }else{
-                console.log("imagem copiada com sucesso!");
-            }
-        })
-    }
-})
+fs.createReadStream(path)
+    .pipe(fs.createWriteStream('./assets/pet02-copy.jpg'))
+    .on('finish',()=>console.log('all right!'))
+    
