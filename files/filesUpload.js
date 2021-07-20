e3357444dcd882e17a34d6447a9849c8ccfa372a
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-07-20 12:27:13 -0300

    write file fs

--- files/filesUpload.js
+++ files/filesUpload.js
@@ -1,12 +1,21 @@
 const fs = require('fs')
 const path = './assets/pet01.jpg'
 
+function handleError(error){
+    console.log("Error------------------------------------");
+    console.log(error);
+}
 fs.readFile(path,(error, buffer)=>{
     if(error){
-        console.log("Erro");
-        console.log(error);
+        handleError(error)
     }else{
         console.log("Image was buffered");
-        console.log(buffer);
+        fs.writeFile('./assets/pet01-copy.jpg',buffer,(error)=>{
+            if(error){
+                handleError(error)
+            }else{
+                console.log("imagem copiada com sucesso!");
+            }
+        })
     }
 })
