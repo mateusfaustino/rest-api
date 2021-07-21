bc0359c206bf03095bffd0eee9b3517000a93f09
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-07-21 14:29:10 -0300

    delete handle error

--- files/filesUpload.js
+++ files/filesUpload.js
@@ -2,10 +2,7 @@ const fs = require('fs')
 const pathModule = require('path')
 
 module.exports = (path, fileName,callback)=>{
-    function handleError(error){
-        console.log("Error------------------------------------");
-        console.log(error);
-    }
+
     const fileExtension = pathModule.extname(path)
     const newPath=`./assets/images/${fileName}${fileExtension}`
     fs.createReadStream(path)
