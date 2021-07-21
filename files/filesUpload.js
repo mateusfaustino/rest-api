834f60913029ebf587a89eeccad698016b231e60
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-07-21 14:36:51 -0300

    valid extension

--- files/filesUpload.js
+++ files/filesUpload.js
@@ -2,10 +2,17 @@ const fs = require('fs')
 const pathModule = require('path')
 
 module.exports = (path, fileName,callback)=>{
-
+    const validExtensions = ['png','jpg','jpeg']
     const fileExtension = pathModule.extname(path)
+    const isExtensionValid = validExtensions.indexOf(fileExtension.substring(1)) !==-1
     const newPath=`./assets/images/${fileName}${fileExtension}`
-    fs.createReadStream(path)
-        .pipe(fs.createWriteStream(newPath))
-        .on('finish',()=>callback(newPath))
+    
+    if(isExtensionValid){
+        fs.createReadStream(path)
+            .pipe(fs.createWriteStream(newPath))
+            .on('finish',()=>callback(newPath))
+    }else{
+        console.log("moiô")
+    }
+    
 }
