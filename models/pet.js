15184d47aaa5a96a3a08236d10afcc1f8e9af881
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-07-21 14:07:19 -0300

    update add pet feature

--- models/pet.js
+++ models/pet.js
@@ -1,14 +1,18 @@
-const conexao =require('../infraestrutura/conection');
+const conexao = require('../infraestrutura/conection');
+const fileUpload = require('../files/filesUpload')
 class Pet{
     add(pet, res){
-        const sql = 'INSERT INTO pets SET ?'
-        conexao.query(sql, pet, (error, results)=>{
-            if (error) {
-                res.status(400).json(error);
-            } else {
-                res.status(201).json(pet);
-            }
-        })            
+        fileUpload(pet.image, pet.name,(newPath)=>{
+            const sql = 'INSERT INTO pets SET ?'
+            newPet = {name:pet.name, image:newPath}
+            conexao.query(sql, newPath, (error, results)=>{
+                if (error) {
+                    res.status(400).json(error);
+                } else {
+                    res.status(201).json(newPath);
+                }
+            })
+        })                    
     }
 
 }
